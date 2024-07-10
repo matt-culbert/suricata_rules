646cb9f3b6f0b9ef9090aeb07afe9145b70a8a65
matt-culbert <culbertmatt@gmail.com>
2024-07-10 16:13:56 -0400

    Update lua_setup.sh

--- lua_setup.sh
+++ lua_setup.sh
@@ -20,13 +20,13 @@ echo "Getting subcomponents"
 git clone https://github.com/OISF/libhtp suricata/
 cargo install --force cbindgen
 
-function autogens () {
+alias autogens '
   cd suricata/
   echo "Performing autogens"
   ./autogen.sh
-}
+'
 
-function linkers () {
+alias linkers '
   echo "Creating links"
   ln -s /usr/local/include/lua54/lua.h /usr/include/lua.h 
   ln -s /usr/local/include/lua54/lualib.h /usr/include/lualib.h 
@@ -36,23 +36,24 @@ function linkers () {
   ln -s /usr/local/lib/liblua-5.4.a /usr/local/lib/liblua54.a 
   ln -s /usr/local/lib/liblua-5.4.so /usr/local/lib/liblua54.so
   ln -s /usr/local/libdata/pkgconfig/lua-5.4.pc /usr/local/libdata/pkgconfig/lua.pc
-}
+'
 
-function flags () {
+alias flags '
   echo "Setting compiler flags"
   setenv LUA_CFLAGS "-I/usr/local/include/lua5.4"
   setenv LUA_LIBS "-L/usr/local/lib -llua-5.4"
-}
+'
 
-function conf () {
+alias conf '
   echo "Running configure"
   ./configure --enable-lua --with-lua=/usr/local/lib
-}
+'
 
-function maker () {
+alias maker '
   echo "Running make"
   make && make install-full
-}
+'
+
 autogens
 linkers
 flags
