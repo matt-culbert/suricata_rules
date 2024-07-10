767de79a3e2ea28181142c5bdcb7ce00e9ab2ad7
matt-culbert <culbertmatt@gmail.com>
2024-07-10 16:38:56 -0400

    Update lua_setup.sh

--- lua_setup.sh
+++ lua_setup.sh
@@ -16,16 +16,6 @@ wget https://github.com/OISF/suricata/archive/refs/tags/suricata-7.0.6.tar.gz
 mkdir suricata
 tar -xzvf suricata-7.0.6.tar.gz -C suricata --strip-components
 
-echo "Getting subcomponents"
-git clone https://github.com/OISF/libhtp suricata/
-cargo install --force cbindgen
-
-alias autogens '\
-  cd suricata/; \
-  echo "Performing autogens"; \
-  ./autogen.sh \
-'
-
 alias linkers '\
   echo "Creating links"; \
   ln -sf /usr/local/include/lua54/lua.h /usr/include/lua.h; \
@@ -38,25 +28,26 @@ alias linkers '\
   ln -sf /usr/local/libdata/pkgconfig/lua-5.4.pc /usr/local/libdata/pkgconfig/lua.pc \
 '
 
-alias flags '\
+alias build '\
+  cd ~/suricata/; \
+  echo "Getting subcomponents"; \
+  git clone https://github.com/OISF/libhtp; \
+  cargo install --force cbindgen; \
+  
+  echo "Performing autogens"; \
+  ./autogen.sh; \
+  
   echo "Setting compiler flags"; \
   setenv LUA_CFLAGS "-I/usr/local/include/lua5.4"; \
-  setenv LUA_LIBS "-L/usr/local/lib -llua-5.4" \
-'
+  setenv LUA_LIBS "-L/usr/local/lib -llua-5.4"; \
 
-alias conf '\
   echo "Running configure"; \
-  ./configure --enable-lua --with-lua=/usr/local/lib \
-'
+  ./configure --enable-lua --with-lua=/usr/local/lib; \
 
-alias maker '\
   echo "Running make"; \
   make && make install-full \
 '
 
-autogens
 linkers
-flags
-conf
-maker
+build
 echo "All done, run source ~/.cshrc"
