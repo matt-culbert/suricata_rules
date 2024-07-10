4012c709def3acc8183c77b253d6aa3b7bc0aa27
matt-culbert <culbertmatt@gmail.com>
2024-07-10 15:53:10 -0400

    Update lua_setup.sh

--- lua_setup.sh
+++ lua_setup.sh
@@ -1,13 +1,10 @@
 #!/bin/tcsh
 
 echo "Downloading pkg components"
-pkg install lua54 autoconf automake libtool pkgconf wget
+pkg install lua54 autoconf automake libtool pkgconf wget git
 
-(
-  echo "Downloading Rust"
-  curl https://sh.rustup.rs | sh
-  echo "1"
-)
+echo "Downloading Rust"
+curl https://sh.rustup.rs | sh
 
 source ~/.cshrc
 
@@ -21,33 +18,45 @@ mkdir suricata
 tar -xzvf suricata-7.0.6.tar.gz -C suricata --strip-components
 
 echo "Getting subcomponents"
-git clone https://github.com/OISF/libhtp
+git clone https://github.com/OISF/libhtp suricata/
 cargo install --force cbindgen
 
-echo "Performing autogens"
-./autogen.sh
+function autogens(){
+  cd suricata/
+  echo "Performing autogens"
+  ./autogen.sh
+}
 
-echo "Creating links"
+function linkers(){
+  echo "Creating links"
+  ln -s /usr/local/include/lua54/lua.h /usr/include/lua.h 
+  ln -s /usr/local/include/lua54/lualib.h /usr/include/lualib.h 
+  ln -s /usr/local/include/lua54/lauxlib.h /usr/include/lauxlib.h 
+  ln -s /usr/local/include/lua54/luaconf.h /usr/include/luaconf.h
+  ln -s /usr/local/include/lua54/ /usr/local/include/lua
+  ln -s /usr/local/lib/liblua-5.4.a /usr/local/lib/liblua54.a 
+  ln -s /usr/local/lib/liblua-5.4.so /usr/local/lib/liblua54.so
+  ln -s /usr/local/libdata/pkgconfig/lua-5.4.pc /usr/local/libdata/pkgconfig/lua.pc
+}
 
-ln -s /usr/local/include/lua54/lua.h /usr/include/lua.h 
-ln -s /usr/local/include/lua54/lualib.h /usr/include/lualib.h 
-ln -s /usr/local/include/lua54/lauxlib.h /usr/include/lauxlib.h 
-ln -s /usr/local/include/lua54/luaconf.h /usr/include/luaconf.h
-ln -s /usr/local/include/lua54/ /usr/local/include/lua
+function flags(){
+  echo "Setting compiler flags"
+  setenv LUA_CFLAGS "-I/usr/local/include/lua5.4"
+  setenv LUA_LIBS "-L/usr/local/lib -llua-5.4"
+}
 
-ln -s /usr/local/lib/liblua-5.4.a /usr/local/lib/liblua54.a 
-ln -s /usr/local/lib/liblua-5.4.so /usr/local/lib/liblua54.so
+function conf(){
+  echo "Running configure"
+  ./configure --enable-lua --with-lua=/usr/local/lib
+}
 
-ln -s /usr/local/libdata/pkgconfig/lua-5.4.pc /usr/local/libdata/pkgconfig/lua.pc
-
-echo "Setting compiler flags"
-setenv LUA_CFLAGS "-I/usr/local/include/lua5.4"
-setenv LUA_LIBS "-L/usr/local/lib -llua-5.4"
-
-echo "Running configure"
-./configure --enable-lua --with-lua=/usr/local/lib
-
-echo "Running make"
-make && make install-full
-
+function maker(){
+  echo "Running make"
+  make && make install-full
+}
+autogens
+linkers
+flags
+conf
+maker
 echo "All done, run source ~/.cshrc"
